e3e5f5e0968df05c53496c212630b55448e442a7
emilio.araya <emiliosaraya@gmail.com>
2025-10-08 23:15:51 -0300

    1er commit

--- Tarea3_java/objetos/Item.java
+++ Tarea3_java/objetos/Item.java
@@ -4,7 +4,7 @@ public class Item {
     private final ItemTipo tipo;
     private final int cantidad;
 
-    public Item(ItemTipo, int cantidad){
+    public Item(ItemTipo tipo, int cantidad){
         if (cantidad <= 0) throw new IllegalArgumentException("cantidad <= 0");
         this.tipo = tipo;
         this.cantidad = cantidad;
--- Tarea3_java/objetos/ItemTipo.java
+++ Tarea3_java/objetos/ItemTipo.java
@@ -1,5 +1,29 @@
 package objetos; 
 
 public enum ItemTipo {
+    // Arrecife
+    CUARZO,
+    SILICIO,
+    COBRE,
 
+    // Profunda
+    PLATA,
+    ORO,
+    ACERO,
+    DIAMANTE,
+    MAGNETITA,
+
+    // Volcánica
+    TITANIO,
+    SULFURO,
+    URANIO,
+
+    // Nave Estrellada (loot con traje)
+    CABLES,
+    PIEZAS_METAL,
+
+    // Progresión / únicos
+    PIEZA_TANQUE,
+    MODULO_PROFUNDIDAD,
+    PLANO_NAVE
 }
